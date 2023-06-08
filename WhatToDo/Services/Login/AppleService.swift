4560eaf760f104a0391a8d84ad49bbfce5aba485
kimchulyeon <guinness987@gmail.com>
2023-06-08 17:59:26 +0900

    feat: 애플 로그인 로직 추가

---
+++ WhatToDo/Services/Login/AppleService.swift
@@ -0,0 +1,124 @@
+//
+//  AppleService.swift
+//  WhatToDo
+//
+//  Created by chulyeon kim on 2023/06/08.
+//
+
+import Foundation
+import SwiftUI
+import CryptoKit
+import AuthenticationServices
+import FirebaseAuth
+
+class AppleService: NSObject, ASAuthorizationControllerDelegate {
+    static let shared = AppleService()
+    
+    var loginView: LoginView!
+
+    private override init() { }
+
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
+
+    // https://firebase.google.com/docs/auth/ios/apple?hl=ko&authuser=0&_gl=1*1hdqmt7*_ga*MTMxMTIxNjg5OC4xNjc4Njc5MTY5*_ga_CW55HF8NVT*MTY4NTUxNTY1NC43LjEuMTY4NTUxNzkwNy4wLjAuMA..
+    func startSignInWithAppleFlow(view: LoginView) {
+        self.loginView = view
+        
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
+
+
+
+    // https://firebase.google.com/docs/auth/ios/apple?hl=ko&authuser=0&_gl=1*1hdqmt7*_ga*MTMxMTIxNjg5OC4xNjc4Njc5MTY5*_ga_CW55HF8NVT*MTY4NTUxNTY1NC43LjEuMTY4NTUxNzkwNy4wLjAuMA..
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+        if errorCode != errSecSuccess {
+            fatalError(
+                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+        }
+
+        let charset: [Character] =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+        let nonce = randomBytes.map { byte in
+            // Pick a random character from the set, wrapping around if needed.
+            charset[Int(byte) % charset.count]
+        }
+
+        return String(nonce)
+    }
+
+    // DELEGATE
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            
+            let email = appleIDCredential.email ?? ""
+            
+            var name = "홍길동"
+            if let fullName = appleIDCredential.fullName {
+                let formatter = PersonNameComponentsFormatter()
+                name = formatter.string(from: fullName)
+            }
+            
+            // Initialize a Firebase credential, including the user's full name.
+            let credential = OAuthProvider.appleCredential(withIDToken: idTokenString,
+                                                           rawNonce: nonce,
+                                                           fullName: appleIDCredential.fullName)
+            // Sign in with Firebase.
+//            Auth.auth().signIn(with: credential) { (authResult, error) in
+//                if let error = error {
+//                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+//                    // you're sending the SHA256-hashed nonce as a hex string with
+//                    // your request to Apple.
+//                    print(error.localizedDescription)
+//                    return
+//                }
+//                // User is signed in to Firebase with Apple.
+//                // ...
+//            }
+//            self.loginView.connectToFirebase(name: name, email: email, provider: "apple", credential: credential)
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+//        self.loginView.isShowErrorAlert.toggle()
+    }
+
+}
+
